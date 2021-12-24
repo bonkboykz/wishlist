06e69e8b4b9a42f7f68827603c3bf5b683bc5da2
Yesset <poxerface5@gmail.com>
2021-12-24 19:14:30 +0600

    chore(#2): Setup linters

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'next/core-web-vitals',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-console': 1,
+    'linebreak-style': ['error', 'unix'],
+    // We're not using pretter-plugin, because it contradicts this guide https://prettier.io/docs/en/integrating-with-linters.html
+  },
+};
